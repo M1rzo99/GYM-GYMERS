da2d826510ef9942a38002bac0718cd1ba062ba9
lucas <accaunoff99@gmail.com>
2025-05-18 12:01:33 +0900

    Fix: make mobile ver and some links

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,19 @@ import Home from './pages/home'
 import Auth from './pages/auth'
 import Dashboard from './pages/dashboard'
 import { Toaster } from './components/ui/sonner'
-
+import { useNavigate } from 'react-router-dom'
+import { useUserState } from '@/store/user.store'
+import { useEffect } from 'react'
 
 const App = () => {
+  const { user } = useUserState()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/auth')  // login page ga redirect
+    }
+  }, [user, navigate])
 	return (
 		<>
 		<Navbar/>
